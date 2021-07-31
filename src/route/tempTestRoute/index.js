f7519c8229269390a9d66b5cd2928c2d28fb1d34
Vikas Kumar <your_email@abc.com>
2021-07-31 21:35:58 +0530

    added schema validation

--- src/route/tempTestRoute/index.js
+++ src/route/tempTestRoute/index.js
@@ -1,5 +1,26 @@
 const tempService = require('../../service/temp.service');
 
+const body = {
+  type: 'object',
+  properties: {
+    title: {
+      type: 'string',
+      minLength: 10,
+    },
+  },
+  required: ['title'],
+};
+
+const postResponse = {
+  201: {
+    type: 'object',
+    required: ['id'],
+    properties: {
+      id: { type: 'string' },
+    },
+  },
+};
+
 const route = async (fastify) => {
   // get route  api/v1/test/
   const { getAll, save } = tempService(fastify);
@@ -11,14 +32,18 @@ const route = async (fastify) => {
   });
 
   // get route api/v1/test
-  fastify.post('/', async (request, reply) => {
-    fastify.log.info(`request with body ${request}`);
-    const { title } = request.body;
+  fastify.post(
+    '/',
+    { schema: { body, response: postResponse } },
+    async (request, reply) => {
+      fastify.log.info(`request with body ${request}`);
+      const { title } = request.body;
 
-    const id = await save(title);
+      const id = await save(title);
 
-    reply.code(201).send(id);
-  });
+      reply.code(201).send(id);
+    }
+  );
 };
 
 module.exports = route;
